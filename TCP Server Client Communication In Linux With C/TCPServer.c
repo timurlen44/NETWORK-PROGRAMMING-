a9fe32f15a296550d7442fcba2f88af3ca211665
Timurleng44 <muhammedemintanik63@gmail.com>
2022-05-23 00:11:21 +0300

    Update TCPServer.c

--- TCP Server Client Communication In Linux With C/TCPServer.c
+++ TCP Server Client Communication In Linux With C/TCPServer.c
@@ -11,35 +11,6 @@
 #define PORT 8080
 #define SA struct sockaddr
 char buff[MAX];
-// Function designed for chat between client and server.
-void func(int connfd)
-{
-    
-    int n;
-    // infinite loop for chat
-    for (;;) {
-        bzero(buff, MAX);
-
-        // read the message from client and copy it in buffer
-        read(connfd, buff, sizeof(buff));
-        // print buffer which contains the client contents
-        printf("From client: %s\nEnter the string: ", buff);
-        bzero(buff, MAX);
-        n = 0;
-        // copy server message in the buffer
-        while ((buff[n++] = getchar()) != '\n')
-            ;
-
-        // and send that buffer to client
-        write(connfd, buff, sizeof(buff));
-
-        // if msg contains "Exit" then server exit and chat ended.
-        if (strncmp("exit", buff, 4) == 0) {
-            printf("Server Exit...\n");
-            break;
-        }
-    }
-}
 
 // Driver function
 int main()
