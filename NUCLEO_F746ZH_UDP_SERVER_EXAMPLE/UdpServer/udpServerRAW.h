8acc7282343dea49c81aaf479a27c7455d7934a2
Muhammed Emin TANIK <muhammedemintanik63@gmail.com>
2024-08-25 16:55:26 +0300

    Update udpServerRAW.h

--- NUCLEO_F746ZH_UDP_SERVER_EXAMPLE/UdpServer/udpServerRAW.h
+++ NUCLEO_F746ZH_UDP_SERVER_EXAMPLE/UdpServer/udpServerRAW.h
@@ -1,24 +1,3 @@
-/*
-  ***************************************************************************************************************
-  ***************************************************************************************************************
-  ***************************************************************************************************************
-
-  File:		  udpServerRAW.h
-  Author:     ControllersTech.com
-  Updated:    Jul 23, 2021
-
-  ***************************************************************************************************************
-  Copyright (C) 2017 ControllersTech.com
-
-  This is a free software under the GNU license, you can redistribute it and/or modify it under the terms
-  of the GNU General Public License version 3 as published by the Free Software Foundation.
-  This software library is shared with public for educational purposes, without WARRANTY and Author is not liable for any damages caused directly
-  or indirectly by this software, read more about this on the GNU General Public License.
-
-  ***************************************************************************************************************
-*/
-
-
 #ifndef INC_UDPSERVERRAW_H_
 #define INC_UDPSERVERRAW_H_
 
